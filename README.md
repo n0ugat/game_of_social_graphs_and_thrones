96787ee06fdb1e50ba0e3daed05467478c2df52c
Nikolai <nbenemark@gmail.com>
2025-11-12 14:55:08 +0100

    Add get_links to functions.py; Add type annotation to functions.py; Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # game_of_social_graphs_and_thrones
 Final project about Game of Thrones for the DTU course Social Graphs and Interactions (02805).
+
+## Central Idea:
+### Idea
+Our main idea is analyzing the many connections in the *Game of Thrones* universe.
+
+### Why is it interesting?
+Game of Thrones features a vast universe, rich in characters, cast, episodes, books, lore, etc. The universe was really popularized by the HBO series Game of Thrones, which was adopted by the original book series *A Song of Ice and Fire* by George R. R. Martin. 
+
+### Datasets
+To do our project we downloaded the pages that make up the [Wiki of Westeros](https://gameofthrones.fandom.com/wiki/Westeros).
+
+### Download process
+To download the needed pages, we used the skills we've learned in the course about APIs.
+
+## Preliminary Data Analysis
+### Data size
+* MB:
+* Number of rows:
+* Number of variables:
+
+### Network description
+
+### Analysis text
+
+### Tie between networks and text
+
--- functions.py
+++ functions.py
@@ -3,7 +3,7 @@ import nltk
 import re
 import json
 
-def load_text_files(file, path='GoT_files', newLine = False):    
+def load_text_files(file: str, path: str = 'GoT_files', newLine: bool = False) -> str:
     with open(os.path.join(path, file), 'r', encoding='utf-8', errors='ignore') as f:
         if newLine:
             text = f.read().splitlines()
@@ -12,7 +12,7 @@ def load_text_files(file, path='GoT_files', newLine = False):
 
     return text
 
-def tokenize_text(text):
+def tokenize_text(text: str) -> list:
     tokens = nltk.word_tokenize(text)
 
     # Filter out punctuation and convert to lowercase in one pass
@@ -20,7 +20,7 @@ def tokenize_text(text):
     return tokens
 
 
-def check_categories(text, unique_categories = []):
+def check_categories(text: str, unique_categories: list = []) -> list:
     """Extract wiki [[Category:...]] values from raw text and append to unique_categories.
 
     Uses a regex that handles single or double brackets and is case-insitive.
@@ -40,7 +40,7 @@ def check_categories(text, unique_categories = []):
 
     return unique_categories
 
-def clean_categories(raw_categories, category_map):
+def clean_categories(raw_categories: list, category_map: dict) -> list:
     """Map raw categories to normalized forms and remove duplicates while preserving order.
     
     Args:
@@ -64,14 +64,14 @@ def clean_categories(raw_categories, category_map):
     
     return normalized
 
-def load_category_mapping(file='category_mapping.json', path='data_handling'):
+def load_category_mapping(file: str = 'category_mapping.json', path: str = 'data_handling'):
     with open(os.path.join(path, file), 'r', encoding='utf-8') as f:
         category_map = json.load(f)
     
     return category_map
     
 
-def get_catgories(text, raw_categories = [], category_map = []):
+def get_catgories(text: str, raw_categories: list = [], category_map: list = []) -> list:
     if category_map == []:
         category_map = load_category_mapping()
     if raw_categories == []:
@@ -81,3 +81,15 @@ def get_catgories(text, raw_categories = [], category_map = []):
 
     return cleaned_categories
     
+def get_links(text: str, path: str = 'GoT_files', no_files: bool = True, no_translations: bool = True) -> list:
+
+    f = open(os.path.join(path, text), 'r', encoding='utf-8')
+
+    if no_files and no_translations:
+        links = re.findall(r"\[{2}(?!\w{2,5}(?:-\w{2})?:)(?!File:)(?!Image:).+?\]{2}", f.read())
+    elif no_files:
+        links = re.findall(r"\[{2}(?!File:)(?!Image:).+?\]{2}", f.read())
+    elif no_translations:
+        links = re.findall(r"\[{2}(?!\w{2,5}(?:-\w{2})?:).+?\]{2}", f.read())
+    
+    return links
